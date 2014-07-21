c847187a66f37500846de02cded10401528ef5aa
Tomasz Lipinski <developer@riffdiktator.com>
2014-07-21 23:11:34 +0200

    Konfiguracja LiveReload, Watch, Jade i Compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,40 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    jade = require('gulp-jade'),
+    sass = require('gulp-compass');
 
-gulp.task('default',function(){
+gulp.task('connect',function(){
     connect.server({
         root: 'dist',
-        port: 8888,
+        port: 8889,
         livereload: {
             port: 8888
         }
     })
 });
+gulp.task('jade', function(){
+    gulp.src('./src/pages/**/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./dist/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('sass', function () {
+    gulp.src('./src/sass/**/*.scss')
+        .pipe(sass({
+            config_file: 'config.rb',
+            css: './dist/styles/',
+            sass: './src/sass/'
+        }))
+        .pipe(connect.reload());
+});
+
+gulp.task('watch',function(){
+    gulp.watch(['./src/pages/**/*.jade'], ['jade']);
+    gulp.watch(['./src/sass/**/*.scss'], ['sass']);
+});
+
+gulp.task('default', ['connect', 'watch']);
+
